03536c1d028dc57e71feeb153700bb7464c17c50
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2014-05-19 12:57:06 +0900

    最初のテストをグリーンに

--- TodoList.CommandLine/Program.cs
+++ TodoList.CommandLine/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace TodoList.CommandLine
     {
         static void Main(string[] args)
         {
+            File.WriteAllText("backup.xml", "<TodoList><Todo><Title>買い物</Title><Detail>牛乳と卵を買う</Detail></Todo></TodoList>");
         }
     }
 }
