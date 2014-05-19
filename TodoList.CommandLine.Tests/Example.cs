034ec63e4640bd9f12dd6b29f2134369933a9f30
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2014-05-19 15:27:21 +0900

    テストコードのリファクタリング

--- TodoList.CommandLine.Tests/Example.cs
+++ TodoList.CommandLine.Tests/Example.cs
@@ -21,58 +21,68 @@ namespace TodoList.CommandLine.Tests
                 File.Delete("backup.xml");
         }
 
+        static void Execute(string subcommand, params string[] args)
+        {
+            var startInfo = new ProcessStartInfo("TodoList.exe", subcommand + " " + string.Join(" ", args))
+            {
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            using (var proc = Process.Start(startInfo))
+            {
+                proc.WaitForExit();
+            }
+        }
+
+        enum StreamKind { StandardOutput, StandardError }
+
+        static string ExecuteAndReadStream(StreamKind kind, string subcommand, params string[] args)
+        {
+            var procInfo = new ProcessStartInfo("TodoList.exe", subcommand + " " + string.Join(" ", args))
+            {
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardOutput = kind == StreamKind.StandardOutput,
+                RedirectStandardError = kind == StreamKind.StandardError
+            };
+            using (var proc = new Process { StartInfo = procInfo })
+            {
+                proc.Start();
+                switch (kind)
+                {
+                    case StreamKind.StandardOutput:
+                        return proc.StandardOutput.ReadToEnd();
+                    case StreamKind.StandardError:
+                        return proc.StandardError.ReadToEnd();
+                    default:
+                        throw new Exception("oops!");
+                }
+            }
+        }
+
         [TestCase("買い物", "牛乳と卵を買う", "<TodoList><Todo><Title>買い物</Title><Detail>牛乳と卵を買う</Detail></Todo></TodoList>")]
         [TestCase("買い物メモ", "牛乳と卵", "<TodoList><Todo><Title>買い物メモ</Title><Detail>牛乳と卵</Detail></Todo></TodoList>")]
         public void TODOを追加するとファイルにTODOが出力される(string title, string detail, string expected)
         {
-            var procInfo = new ProcessStartInfo("TodoList.exe", string.Format("add {0} {1}", title, detail));
-            using (var proc = Process.Start(procInfo))
-            {
-                proc.WaitForExit();
-                var backup = XDocument.Load("backup.xml");
-                Assert.That(backup.ToString(SaveOptions.DisableFormatting), Is.EqualTo(expected));
-            }
+            Execute("add", title, detail);
+            var backup = XDocument.Load("backup.xml");
+            Assert.That(backup.ToString(SaveOptions.DisableFormatting), Is.EqualTo(expected));
         }
 
         [Test]
         public void 初期状態で最後に追加されたTODOを見ようとするとエラーが表示される()
         {
-            var procInfo = new ProcessStartInfo("TodoList.exe", "show last")
-            {
-                UseShellExecute = false,
-                CreateNoWindow = true,
-                RedirectStandardError = true
-            };
-            using (var proc = new Process { StartInfo = procInfo })
-            {
-                proc.Start();
-                var output = proc.StandardError.ReadToEnd();
-                Assert.That(output, Is.EqualTo("エラー: TODOがありません。\r\n"));
-            }
+            var output = ExecuteAndReadStream(StreamKind.StandardError, "show", "last");
+            Assert.That(output, Is.EqualTo("エラー: TODOがありません。\r\n"));
         }
 
         [TestCase("買い物メモ", "牛乳と卵", "タイトル: 買い物メモ\r\n詳細: 牛乳と卵\r\n")]
         [TestCase("買い物", "牛乳と卵を買う", "タイトル: 買い物\r\n詳細: 牛乳と卵を買う\r\n")]
         public void Todoを1つ追加した状態で最後に追加されたTODOの詳細が見れる(string title, string detail, string expected)
         {
-            var procInfo = new ProcessStartInfo("TodoList.exe", string.Format("add {0} {1}", title, detail));
-            using (var proc = Process.Start(procInfo))
-            {
-                proc.WaitForExit();
-            }
-
-            procInfo = new ProcessStartInfo("TodoList.exe", "show last")
-            {
-                UseShellExecute = false,
-                CreateNoWindow = true,
-                RedirectStandardOutput = true
-            };
-            using (var proc = new Process { StartInfo = procInfo })
-            {
-                proc.Start();
-                var output = proc.StandardOutput.ReadToEnd();
-                Assert.That(output, Is.EqualTo(expected));
-            }
+            Execute("add", title, detail);
+            var output = ExecuteAndReadStream(StreamKind.StandardOutput, "show", "last");
+            Assert.That(output, Is.EqualTo(expected));
         }
     }
 }
