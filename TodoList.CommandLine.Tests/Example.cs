9382d8ddb1cedcc81648867e767c9f4794761b22
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2014-05-19 12:48:56 +0900

    最初のテストを追加

---
+++ TodoList.CommandLine.Tests/Example.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace TodoList.CommandLine.Tests
+{
+    [TestFixture]
+    public class Example
+    {
+        [Test]
+        public void TODOを追加するとファイルにTODOが出力される()
+        {
+            var procInfo = new ProcessStartInfo("TodoList.exe", "add 買い物 牛乳と卵を買う");
+            using (var proc = Process.Start(procInfo))
+            {
+                proc.WaitForExit();
+                var backup = XDocument.Load("backup.xml");
+                Assert.That(
+                    backup.ToString(SaveOptions.DisableFormatting),
+                    Is.EqualTo("<TodoList><Todo><Title>買い物</Title><Detail>牛乳と卵を買う</Detail></Todo></TodoList>"));
+            }
+        }
+    }
+}
