df261f4beefd205e3494213c11bd7cf4e19ca59a
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2014-05-19 16:12:18 +0900

    リファクタリング

--- TodoList.CommandLine/Program.cs
+++ TodoList.CommandLine/Program.cs
@@ -11,51 +11,45 @@ namespace TodoList.CommandLine
 {
     class Program
     {
+        readonly static FileStorage storage = new FileStorage();
+
         static void Main(string[] args)
         {
-            var storage = new FileStorage();
             var todoTable = new TodoTable(storage);
 
             var command = args[0];
             switch (command)
             {
                 case "add":
-                    {
-                        var title = args[1];
-                        var detail = args[2];
-
-                        var todo = new Todo(title, detail);
-                        todoTable.Add(todo);
-                        storage.Save();
-                        break;
-                    }
+                    Add(todoTable, args[1], args[2]);
+                    break;
                 case "show":
-                    {
-                        var type = args[1];
-                        switch (type)
-                        {
-                            case "first":
-                                {
-                                    var todo = todoTable.GetFirstTodo();
-                                    if (todo == null)
-                                        Console.Error.WriteLine("エラー: TODOがありません。");
-                                    else
-                                        Console.WriteLine(todo);
-                                    break;
-                                }
-                            case "last":
-                                {
-                                    var todo = todoTable.GetLastTodo();
-                                    if (todo == null)
-                                        Console.Error.WriteLine("エラー: TODOがありません。");
-                                    else
-                                        Console.WriteLine(todo);
-                                    break;
-                                }
-                        }
-                        break;
-                    }
+                    Show(todoTable, args[1]);
+                    break;
             }
+        }
+
+        private static void Add(TodoTable todoTable, string title, string detail)
+        {
+            var todo = new Todo(title, detail);
+            todoTable.Add(todo);
+            storage.Save();
+        }
+
+        static readonly IDictionary<string, Func<TodoTable, Todo>> getters = new Dictionary<string, Func<TodoTable, Todo>>
+        {
+            { "first", todoTable => todoTable.GetFirstTodo() },
+            { "last", todoTable => todoTable.GetLastTodo() }
+        };
+
+        private static void Show(TodoTable todoTable, string type)
+        {
+            Func<TodoTable, Todo> getter = getters[type];
+            var todo = getter(todoTable);
+            if (todo == null)
+                Console.Error.WriteLine("エラー: TODOがありません。");
+            else
+                Console.WriteLine(todo);
         }
     }
 }
