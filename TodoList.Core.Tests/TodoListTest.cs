209ea72d78c8920f540ce15ed3ea3e2ffc6cecf3
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2014-05-19 13:16:22 +0900

    作ったものをCoreに移動開始

---
+++ TodoList.Core.Tests/TodoListTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoList.Core;
+
+namespace TodoList.Core.Tests
+{
+    [TestFixture]
+    public class TodoListTest
+    {
+        [Test]
+        public void 空のTodoTableにTodoを追加してその詳細が取得できる()
+        {
+            var todoTable = new TodoTable();
+            todoTable.Add(new Todo("買い物メモ", "牛乳と卵"));
+            Assert.That(todoTable.GetLastTodo(), Is.EqualTo(new Todo("買い物メモ", "牛乳と卵")));
+        }
+    }
+}
---
+++ TodoList.Core/TodoTable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TodoList.Core
+{
+    public class TodoTable
+    {
+        public void Add(Todo todo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Todo GetLastTodo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
